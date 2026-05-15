45db961ed6a306ac4763c272b5bbfe55b7cc3f8f
nessd00dle <reyesvanessa940@gmail.com>
2026-05-14 23:04:11 -0600

    API-URL configurada

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -9,7 +9,7 @@ const apiClient = axios.create({
   },
 });
 
-// Interceptor to add token to requests
+
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
